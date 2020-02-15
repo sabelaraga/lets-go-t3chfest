77d80da72cd172021b1b1223da1cf1ae816f0a23
Sabela <sabelaraga@users.noreply.github.com>
2020-02-15 20:36:19 +0100

    Add files via upload

---
+++ exercises/ims7d8faOuY.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+type MyError struct{
+	msg string
+}
+
+func (e MyError) Error() string{
+	return e.msg
+}
+
+
+type Message struct{
+  txt string
+}
+
+func (m Message) print () error {
+	if m.txt == "" {
+     		return MyError { msg: "fail!"}
+   	}
+
+	fmt.Println(m.txt)
+	return nil	
+}
+
+func NewMessage(s string) Message {
+	return Message{ txt : s }
+}
+
+func main() {
+	m := NewMessage("Tra tra")
+	if err := m.print(); err!= nil {
+		fmt.Printf("Ouch! %t", err)
+	}
+}
