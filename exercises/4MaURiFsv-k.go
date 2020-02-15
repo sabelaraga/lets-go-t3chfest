2660ab99df894ebfd52fa6a86b25df2e0ce32534
Sabela <sabelaraga@users.noreply.github.com>
2020-02-15 19:14:12 +0100

    Add files via upload

---
+++ exercises/4MaURiFsv-k.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Message struct{
+  txt string
+}
+
+func print (/* add parameters */) /* return type */ {
+  /* move content */
+}
+
+func main() {
+	m := Message{ txt : "Tra tra" }
+	fmt.Println(m.txt)
+	/* call function */
+}
